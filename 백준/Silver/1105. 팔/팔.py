d6128356e62ed710fe4e1d3bfc781f9aaeb1a7dd
Hyun ah Rho <81230764+erica0321@users.noreply.github.com>
2023-07-18 23:58:40 +0900

    [Silver I] Title: 팔, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1105. 팔/팔.py
@@ -0,0 +1,15 @@
+l, r = input().split()
+
+answer = 0
+
+if len(l) != len(r):
+    print(0)
+else:
+    for i in range(len(l)):
+        if l[i] == r[i]:
+            if l[i] == '8':
+                answer += 1
+        else:
+            break
+
+    print(answer)
