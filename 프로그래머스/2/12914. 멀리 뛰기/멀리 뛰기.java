1cc578745447e68ad9f99632208d90157eae5c7b
gusdksepdy <81230764+erica0321@users.noreply.github.com>
2024-07-16 17:51:25 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.15 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long solution(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        
+        long[] ways = new long[n + 1];
+        ways[1] = 1;
+        ways[2] = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            ways[i] = (ways[i-1] + ways[i-2]) % 1234567;
+        }
+        
+        return ways[n];
+    }
+}
