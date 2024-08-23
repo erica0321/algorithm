1438b8851f5cdf3ddbc140d2130177f3f5d239ab
gusdksepdy <81230764+erica0321@users.noreply.github.com>
2024-08-23 10:00:22 +0900

    [Silver II] Title: 풍선 공장, Time: 1048 ms, Memory: 110636 KB -BaekjoonHub

--- 백준/Silver/15810. 풍선 공장/풍선 공장.java
+++ 백준/Silver/15810. 풍선 공장/풍선 공장.java
@@ -5,25 +5,25 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-  public static long N = 0;
-  public static long M = 0;
-  public static long[] times;
+  public static int N = 0;
+  public static int M = 0;
+  public static int[] times;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
-    N = Long.parseLong(st.nextToken());
-    M = Long.parseLong(st.nextToken());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
 
-    times = new long[(int)N];
+    times = new int[N];
     String[] str = br.readLine().split(" ");
     for(int i = 0; i<N; i++) {
-      times[i] = Long.parseLong(str[i]);
+      times[i] = Integer.parseInt(str[i]);
     }
 
     Arrays.sort(times);
     long min = 1;
-    long max = times[(int)(N-1)] * M;
+    long max = (long) times[N - 1] * M;
 
     long answer = BSearch(min, max);
     System.out.println(answer);
@@ -32,7 +32,7 @@ public class Main {
   public static long BSearch(long min, long max) {
     long answer = max;
     while(min <= max) {
-      long mid = min + (max - min) / 2; // 수정된 부분
+      long mid = (max + min) / 2;
       long possible = 0;
 
       for(long time: times) {
