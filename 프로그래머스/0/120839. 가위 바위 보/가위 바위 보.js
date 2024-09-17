14fe56961bf78d8d9a10eedcc7569d03ac6c4bc2
gusdksepdy <81230764+erica0321@users.noreply.github.com>
2024-09-17 19:19:57 +0900

    [level 0] Title: 가위 바위 보, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,9 @@
+function solution(rsp) {
+    var answer = '';
+    for(let str of rsp) {
+        if(str === '2') answer += '0'
+        else if(str === '0') answer += '5'
+        else if(str === '5') answer += '2'
+    }
+    return answer;
+}
