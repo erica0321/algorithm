d95a00ff5ede9ffd0f2726f5e5cb78c415afd19e
gusdksepdy <81230764+erica0321@users.noreply.github.com>
2024-09-20 15:13:55 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,13 @@
+function solution(my_string) {
+    const arr = my_string.split(" ");
+    let answer = 0;
+    answer += Number(arr[0]);
+    for(let i =0; i< arr.length; i++) {
+        if(arr[i] === '+') {
+            answer += Number(arr[i+1]);
+        } else if(arr[i] === '-') {
+            answer -= Number(arr[i+1]);
+        }
+    }
+    return answer;
+}
