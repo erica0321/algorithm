ed01deb74671769684bc7b9085520b07582287c8
gusdksepdy <81230764+erica0321@users.noreply.github.com>
2024-10-02 09:41:25 +0900

    [Silver V] Title: 돌 게임, Time: 104 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int num = Integer.parseInt(br.readLine());
+
+    if(num % 2 == 1) System.out.println("SK");
+    if(num % 2 == 0) System.out.println("CY");
+  }
+}
