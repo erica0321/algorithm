640a654653af418345cbf3f8594869fd8513060e
gusdksepdy <81230764+erica0321@users.noreply.github.com>
2024-10-17 22:58:45 +0900

    [Silver V] Title: 숫자 카드, Time: 2784 ms, Memory: 158856 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -2,59 +2,46 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-  public static int[] arr;
-  public static int[] check;
-  public static int[] result;
+  public static int[] sang;
+  public static int N;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    int N = Integer.parseInt(br.readLine());
-    arr = new int[N];
+    N = Integer.parseInt(br.readLine());
+    sang = new int[N];
 
     StringTokenizer st = new StringTokenizer(br.readLine());
-    for(int i = 0; i<N; i++) {
-      arr[i] = Integer.parseInt(st.nextToken());
+    for(int i =0; i< N; i++) {
+      sang[i] = Integer.parseInt(st.nextToken());
     }
-
-    Arrays.sort(arr);
+    Arrays.sort(sang);
 
     int M = Integer.parseInt(br.readLine());
-    check = new int[M];
-    result = new int[M];
+    int[] arr = new int[M];
 
     st = new StringTokenizer(br.readLine());
-    for(int i = 0; i<M; i++) {
-      check[i] = Integer.parseInt(st.nextToken());
+    for(int i =0; i<M; i++) {
+      arr[i] = BSearch(Integer.parseInt(st.nextToken()));
     }
 
-
-    for(int i = 0; i<M; i++) {
-      result[i] = BSearch(check[i]);
-    }
-
-    for(int i = 0; i<M; i++) {
-      System.out.print(result[i] + " ");
+    for(int num: arr) {
+      System.out.print(num + " ");
     }
   }
 
   public static int BSearch(int num) {
     int min = 0;
-    int max = arr.length - 1;
+    int max = N-1;
 
-    // 이진 탐색
-    while (min <= max) {
-      int mid = (min + max) / 2;
-      if (arr[mid] == num) {
-        return 1;
-      } else if (arr[mid] < num) {
-        min = mid + 1;
+    while(min <= max) {
+      int mid = (max+min)/2;
+      if(num == sang[mid]) { return 1 ;}
+      else if(num < sang[mid]) {
+        max = mid -1;
       } else {
-        max = mid - 1;
+        min =mid + 1;
       }
     }
     return 0;
