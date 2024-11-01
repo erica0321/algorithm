29579ea372852bd6cff2230c96e6f34468f14985
gusdksepdy <81230764+erica0321@users.noreply.github.com>
2024-11-01 17:08:54 +0900

    [level 3] Title: 아이템 줍기, Time: 1.05 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,62 +1,61 @@
 import java.util.*;
 
 class Solution {
-    public static int result;
-    public static int[][] map = new int[101][101];
-    public static boolean[][] visited = new boolean[101][101];
+    public static int[] start, end;
     public static int[] dx = {-1, 1, 0, 0};
     public static int[] dy = {0, 0, -1, 1};
-
+    public static int answer = Integer.MAX_VALUE;
+    public static int[][] map;
+    public static boolean[][] visited;
+    
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-        for (int i = 0; i < rectangle.length; i++) {
-            int x1 = rectangle[i][0];
-            int y1 = rectangle[i][1];
-            int x2 = rectangle[i][2];
-            int y2 = rectangle[i][3];
-
-            for (int y = y1 * 2; y <= y2 * 2; y++) {
-                for (int x = x1 * 2; x <= x2 * 2; x++) {
-                    if (map[y][x] == 1) continue;
-                    map[y][x] = 1;
-                    if (y == y1 * 2 || y == y2 * 2 || x == x1 * 2 || x == x2 * 2) {
-                        map[y][x] = 2;  
-                    }
+        map = new int[101][101];
+        
+        start = new int[]{characterX, characterY};
+        end = new int[] {itemX, itemY};
+        
+        for (int[] rect : rectangle) {
+            for (int i = rect[1] * 2; i <= rect[3] * 2; i++) {
+                for (int j = rect[0] * 2; j <= rect[2] * 2; j++) {
+                    if (map[i][j] == 2) continue;
+                    map[i][j] = 2;
+                    if (i == rect[1] * 2 || i == rect[3] * 2 || j == rect[0] * 2 || j == rect[2] * 2) map[i][j] = 1;
                 }
             }
         }
-
-        result = 0;
-        bfs(characterX * 2, characterY * 2, itemX * 2, itemY * 2);  
-        return result / 2; 
+        
+        visited = new boolean[101][101];
+        
+        bfs(start[0] * 2, start[1] * 2, 0);
+        
+        return answer / 2;
     }
-
-    public static void bfs(int startX, int startY, int itemX, int itemY) {
+    
+    public static void bfs(int x, int y, int count) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{startX, startY, 0}); 
-        visited[startY][startX] = true;
+        queue.add(new int[] {x, y, count});
+        visited[y][x] = true;
+        
+       while(!queue.isEmpty()) {
+           int[] cur = queue.poll();
+           int cx = cur[0];
+           int cy = cur[1];
+           int length = cur[2];
+           
+           if(cx == end[0] *2 && cy == end[1]*2) {
+               answer = length;
+               return;
+           }
+           
+           for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
 
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int x = cur[0];
-            int y = cur[1];
-            int dist = cur[2];
-
-            if (x == itemX && y == itemY) {
-                result = dist;
-                return;
-            }
-
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if (nx >= 0 && nx < 101 && ny >= 0 && ny < 101 && !visited[ny][nx] && map[ny][nx] == 2) {
+                if (nx >= 0 && nx < 101 && ny >= 0 && ny < 101 && !visited[ny][nx] && map[ny][nx] == 1) {
                     visited[ny][nx] = true;
-                    queue.add(new int[]{nx, ny, dist + 1}); 
+                    queue.add(new int[]{nx, ny, length + 1}); 
                 }
             }
-        }
-        result = 0;
+       }
     }
-
 }
