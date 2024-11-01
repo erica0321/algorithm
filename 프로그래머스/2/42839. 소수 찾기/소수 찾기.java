9569aeb825c3646e0fd4240e418e8b585ab2b903
gusdksepdy <81230764+erica0321@users.noreply.github.com>
2024-11-01 12:39:33 +0900

    [level 2] Title: 소수 찾기, Time: 15.08 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,49 +1,42 @@
 import java.util.*;
 
 class Solution {
-    public static int N;
-    public static boolean[] visited;
+    public static int answer = 0;
     public static Set<Integer> set;
-    public static String[] numberArr;
+    public static boolean[] visited;
+    public static String[] str;
     
     public int solution(String numbers) {
-        int answer = 0;
-        numberArr = numbers.split("");
-        
-        N = numberArr.length;
-        visited = new boolean[N];
-        
         set = new HashSet<>();
-    
-        dfs(0, "");
+        visited = new boolean[numbers.length()];
+        str = numbers.split("");
         
-        for(Integer num: set) {
+        
+        dfs("");
+        
+        for(int num : set) {
             if(isPrime(num)) answer ++;
         }
+        
         return answer;
     }
-                
     
-    public static void dfs(int depth, String str) {
-        if(depth == N) return;
-        
-        for(int i =0; i<N; i++) {
+    public static boolean isPrime(int num) {
+        if(num < 2) return false;
+        for(int i = 2; i<= Math.sqrt(num); i++) {
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+    
+    public static void dfs(String make) {
+        if(!make.equals("")) set.add(Integer.parseInt(make));
+        for(int i =0; i<visited.length; i++) {
             if(!visited[i]) {
                 visited[i] = true;
-                String newStr = str + numberArr[i];
-                System.out.print(newStr + " ");
-                set.add(Integer.parseInt(newStr));
-                dfs(depth+1, newStr);
+                dfs(make + str[i]);
                 visited[i] = false;
             }
         }
-    }
-    
-    public static boolean isPrime(int n) {
-        if(n < 2) return false;
-        for(int i = 2; i <= Math.sqrt(n); i++) {
-            if(n % i == 0) return false;
-        }
-        return true;
     }
 }
